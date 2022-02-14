33d5da803efbfacd484173880efdd4e2a5b60b3d
Shawn Witemyre <switemyre@comcast.net>
2022-02-13 22:39:46 -0500

    Todo-App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app';
+  listOfTasks: any[] = [];
+
+  addTask(task: string) {
+    console.warn(task);
+    this.listOfTasks.push({ id: this.listOfTasks.length, name: task });
+    console.warn(this.listOfTasks);
+  }
+
+  removeTask(id: number) {
+    this.listOfTasks = this.listOfTasks.filter((item) => item.id !== id);
+  }
 }
